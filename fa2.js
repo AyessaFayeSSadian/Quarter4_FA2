78268df2ffcf378dd5a3940b46f46978749a25ac
AyessaFayeSSadian <ayessafaye.sadian@smc.pshs.edu.ph>
2025-04-15 04:50:27 +0800

    Add files via upload

---
+++ fa2.js
@@ -0,0 +1,38 @@
+ function multitable() {
+	 let table;
+     table   =   "<table>";
+     let countRows       =   document.getElementById("countRows").value;
+     let countColumns    =   document.getElementById("countColumns").value;
+
+	let counts = [
+		countRows,
+		countColumns
+    ]
+
+	let count   =   {
+		Rows:      document.getElementById("countRows").value,
+		Columns:   document.getElementById("countColumns").value
+	}
+
+
+	for (num of counts) {
+		if (num <= 2 || num >= 10) {
+			window.alert("Please enter values between 2 and 10");
+			return;
+		}
+    }
+
+	for (len = 1; len   <=   count.Rows; len++) {
+    table += "<tr>";
+
+		for (i  =   1; i    <=   count.Columns; i++) {
+			table   +=  "<td>" + i * len + "</td>";
+		} 
+
+		table += "</tr>";
+	}
+
+	table   +=  "</table>";
+	document.getElementById("multiplicationTable").innerHTML = table;
+ }
+   
